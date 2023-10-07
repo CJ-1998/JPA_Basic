84327cd66072569e209c2d2ce6328860cb0deb39
CJ <choijun0627@konkuk.ac.kr>
2023-10-07 16:36:23 +0900

    <feat> JPA 기본편 프로젝트 section 9
    임베디드 타입과 불변 객체
    
    임베디드 타입을 =로 하면 참조 넘어가서 한번에 같이 바뀐다.
    그래서 생성자를 만들고 setter를 제거해서 불변 객체로 만들어서 사용해야 한다.

--- src/main/java/hellojpa/Address.java
+++ src/main/java/hellojpa/Address.java
@@ -12,27 +12,31 @@ public class Address {
     public Address() {
     }
 
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
     public String getCity() {
         return city;
     }
-
-    public void setCity(String city) {
-        this.city = city;
+    public String getZipcode() {
+        return zipcode;
     }
-
     public String getStreet() {
         return street;
     }
 
-    public void setStreet(String street) {
-        this.street = street;
-    }
+//    public void setCity(String city) {
+//        this.city = city;
+//    }
 
-    public String getZipcode() {
-        return zipcode;
-    }
+//    public void setStreet(String street) {
+//        this.street = street;
+//    }
 
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
+//    public void setZipcode(String zipcode) {
+//        this.zipcode = zipcode;
+//    }
 }
