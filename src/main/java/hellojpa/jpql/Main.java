d831248b5a3b9e90c2c860a9d7ccf96fb4b1cbba
CJ <choijun0627@konkuk.ac.kr>
2023-10-10 22:10:59 +0900

    <feat> JPA 기본편 프로젝트 section 10
    프로젝션
    
    여러 타입의 프로젝션과 스칼라 타입 프로젝션에서 여러 값 조회할 때 어떻게
    해야 하는지 알아보기 위한 10_3 함수 생성
    그리고 여러 값 조회할 때 필요한 MemberDTO1 클래스 생성

--- src/main/java/hellojpa/jpql/Main.java
+++ src/main/java/hellojpa/jpql/Main.java
@@ -1,6 +1,7 @@
 package hellojpa.jpql;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class Main {
 
@@ -13,18 +14,7 @@ public class Main {
         tx.begin();
 
         try {
-            Member1 member1=new Member1();
-            member1.setUsername("kim");
-            member1.setAge(10);
-            em.persist(member1);
-
-            TypedQuery<Member1> query1 = em.createQuery("SELECT m FROM Member1 m", Member1.class);
-            Query query2 = em.createQuery("SELECT m.username, m.age from Member1 m");
-
-            TypedQuery<Member1> query3 = em.createQuery("SELECT m FROM Member1 m where m.username = :username", Member1.class);
-            query3.setParameter("username","kim");
-            Member1 singleResult = query3.getSingleResult();
-            System.out.println("singleResult.getUsername() = " + singleResult.getUsername());
+            JPALecture10_2(em);
 
             tx.commit();
         }
@@ -36,5 +26,53 @@ public class Main {
         }
 
         emf.close();
+    }
+
+    public static void JPALecture10_2(EntityManager em){
+        //jpql 알아보기
+        Member1 member1=new Member1();
+        member1.setUsername("kim");
+        member1.setAge(10);
+        em.persist(member1);
+
+        TypedQuery<Member1> query1 = em.createQuery("SELECT m FROM Member1 m", Member1.class);
+        Query query2 = em.createQuery("SELECT m.username, m.age from Member1 m");
+
+        TypedQuery<Member1> query3 = em.createQuery("SELECT m FROM Member1 m where m.username = :username", Member1.class);
+        query3.setParameter("username","kim");
+        Member1 singleResult = query3.getSingleResult();
+        System.out.println("singleResult.getUsername() = " + singleResult.getUsername());
+    }
+
+    public static void JPALecture10_3(EntityManager em){
+        //프로젝션
+        Member1 member1=new Member1();
+        member1.setUsername("kim");
+        member1.setAge(10);
+        em.persist(member1);
+
+        em.flush();
+        em.clear();
+
+        //엔티티 프로젝션 1
+        List<Member1> result = em.createQuery("SELECT m FROM Member1 m", Member1.class).getResultList();
+
+        Member1 findMember = result.get(0);
+        findMember.setAge(20);
+
+        //엔티티 프로젝션 2
+        em.createQuery("SELECT m.team FROM Member1 m", Team1.class).getResultList();
+
+        //임베디드 타입 프로젝션
+        em.createQuery("SELECT o.address FROM Order1 o", Address1.class).getResultList();
+
+        //스칼라 타입 프로젝션
+        em.createQuery("SELECT distinct m.username, m.age FROM Member1 m").getResultList();
+
+        //스칼라 타입 프로젝션에서 여러 값 조회
+        List<MemberDTO1> resultList = em.createQuery("SELECT new jpql.MemberDTO1(m.username, m.age) FROM Member1 m", MemberDTO1.class).getResultList();
+        MemberDTO1 memberDTO1=resultList.get(0);
+        System.out.println("memberDTO1.getUsername() = " + memberDTO1.getUsername());
+        System.out.println("memberDTO1.getAge() = " + memberDTO1.getAge());
     }
 }
---
+++ src/main/java/hellojpa/jpql/MemberDTO1.java
@@ -0,0 +1,28 @@
+package hellojpa.jpql;
+
+public class MemberDTO1 {
+
+    private String username;
+    private int age;
+
+    public MemberDTO1(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
